9c848975beab0e109eaeef527c189f9704df85f0
Pekka Pekkonen <pekka.pekkonen@live.fi>
2016-10-23 12:59:06 +0300

    Encapsulate data printing to separate function

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-function getWeather() {
+function getWeather(callback) {
     if(req.readyState === XMLHttpRequest.DONE && req.status === 200) {
         var weatherData = JSON.parse(req.responseText);
-        city = weatherData.name;
-        temperature = weatherData.main.temp;
+        callback(weatherData);
     }
 }
 
+function showWeather(weatherData) {
+    console.log(weatherData.name);
+    console.log(weatherData.main.temp);
+}
+
 var req = new XMLHttpRequest();
-req.addEventListener('readystatechange', getWeather);
+req.addEventListener('readystatechange', function() {getWeather(showWeather)});
 req.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=vantaa&units=metric&appid=' + apikey);
 req.send();
 
