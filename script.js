308863ac9807eefd756cdb67fe0dc90232a27065
Pekka Pekkonen <pekka.pekkonen@live.fi>
2016-10-27 17:24:47 +0300

    Print related weather icon from API

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function printData(searchedCity) {
     ajax('http://api.openweathermap.org/data/2.5/weather?q=' + searchedCity + '&units=metric&appid=' + apikey, function(weatherData) {
         document.getElementById('searchedCity').innerHTML = weatherData.name;
         document.getElementById('searchedCityTemperature').innerHTML = weatherData.main.temp;
+        document.getElementById('weatherIcon').setAttribute('src', 'http://openweathermap.org/img/w/' + weatherData.weather[0].icon + '.png');
     });
 }
 
