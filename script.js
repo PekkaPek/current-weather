fcd6d85d73500e627e2bd45c7466ab741b10fce7
Pekka Pekkonen <pekka.pekkonen@live.fi>
2016-10-29 23:44:13 +0300

    Change tab size

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 function getElem(id) {
-    return document.getElementById(id);
+  return document.getElementById(id);
 }
 
 function ajax(endPoint, callback) {
-    if (typeof callback !== 'function') {
-        throw new Error('Callback provided is not a function');
+  if (typeof callback !== 'function') {
+    throw new Error('Callback provided is not a function');
+  }
+  var req = new XMLHttpRequest();
+  req.addEventListener('readystatechange', function () {
+    if (req.readyState === XMLHttpRequest.DONE && req.status === 200) {
+      callback(JSON.parse(req.responseText));
     }
-    var req = new XMLHttpRequest();
-    req.addEventListener('readystatechange', function () {
-        if (req.readyState === XMLHttpRequest.DONE && req.status === 200) {
-            callback(JSON.parse(req.responseText));
-        }
-    });
-    req.open('GET', endPoint);
-    req.send();
+  });
+  req.open('GET', endPoint);
+  req.send();
 }
 
 function printData(searchedCity) {
-    getElem('searchedCity').innerHTML = 'Loading';
-    ajax('http://api.openweathermap.org/data/2.5/weather?q=' + searchedCity + '&units=metric&appid=' + apikey, function (weatherData) {
-        getElem('searchedCity').innerHTML = weatherData.name;
-        getElem('searchedCityTemperature').innerHTML = Math.round(weatherData.main.temp) + ' &deg;C';
-        getElem('weatherIcon').setAttribute('src', 'http://openweathermap.org/img/w/' + weatherData.weather[0].icon + '.png');
-    });
+  getElem('searchedCity').innerHTML = 'Loading';
+  ajax('http://api.openweathermap.org/data/2.5/weather?q=' + searchedCity + '&units=metric&appid=' + apikey, function (weatherData) {
+    getElem('searchedCity').innerHTML = weatherData.name;
+    getElem('searchedCityTemperature').innerHTML = Math.round(weatherData.main.temp) + ' &deg;C';
+    getElem('weatherIcon').setAttribute('src', 'http://openweathermap.org/img/w/' + weatherData.weather[0].icon + '.png');
+  });
 }
 
 getElem('searchForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    var cityField = getElem('cityField');
-    var searchedCity = cityField.value.trim();
-    cityField.value = '';
-    if (searchedCity) {
-        printData(searchedCity);
-    }
+  e.preventDefault();
+  var cityField = getElem('cityField');
+  var searchedCity = cityField.value.trim();
+  cityField.value = '';
+  if (searchedCity) {
+    printData(searchedCity);
+  }
 });
